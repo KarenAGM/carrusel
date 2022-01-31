15574a1be30ae0e57460a514f7403d7a7127cf1d
KarenAGM <kagm970412@gmail.com>
2022-01-31 00:32:34 -0600

    Se agrego codigo JS

---
+++ main.js
@@ -0,0 +1,14 @@
+const fila = document.getElementById('contenedor_carrusel');
+
+const flecha_izquierda = document.getElementById('flecha_izquierda');
+const flecha_derecha = document.getElementById('flecha_derecha');
+
+flecha_derecha.addEventListener('click',() => {
+
+    fila.scrollLeft += 245;
+});
+
+flecha_izquierda.addEventListener('click',() => {
+
+    fila.scrollLeft -= 245;
+});
