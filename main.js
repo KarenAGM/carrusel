24227f69a7dea63df264035dbbd2a7fc7f471e70
KarenAGM <kagm970412@gmail.com>
2022-03-03 10:29:47 -0600

    Se modifico js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const slider = document.querySelector(".carrusel");
+
+slider.innerHTML += slider.innerHTML;
+
+
 const fila = document.getElementById('contenedor_carrusel');
 
 const flecha_izquierda = document.getElementById('flecha_izquierda');
@@ -5,10 +10,11 @@ const flecha_derecha = document.getElementById('flecha_derecha');
 
 flecha_derecha.addEventListener('click',() => {
 
-    fila.scrollLeft += 220;
+    fila.scrollLeft += 190;
 });
 
 flecha_izquierda.addEventListener('click',() => {
 
-    fila.scrollLeft -= 220;
+    fila.scrollLeft -= 190;
 });
+
